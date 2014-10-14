d4bf6e86899e0e3c18ede812bc73da0e80bacc8f
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2014-10-14 17:28:59 +0200

    preambolo and folders

--- public_html/app.config.js
+++ public_html/app.config.js
@@ -7,39 +7,39 @@
    angular.module('config',[])
    .config(['$routeProvider', function($routeProvider){
     $routeProvider    
-        .when('/me', {
+        .when('/preambolo', {
             controller : 'appController',
-            templateUrl : 'views/it/home/me.html',
-            title : 'Mauro Vezzoli', 
+            templateUrl : 'views/it/home/preambolo.html',
+            title : 'Preambolo', 
             lang : 'it'
         })       
         .when('/dante', {
             controller : 'appController',
-            templateUrl : 'views/it/content/dante.html',
+            templateUrl : 'views/it/maestri/dante.html',
             title : 'Dante', 
             lang : 'it'
         })
         .when('/monumentoaicaduti', {
             controller : 'appController',
-            templateUrl : 'views/it/content/monumento ai caduti.html',
+            templateUrl : 'views/it/luoghi/monumento ai caduti.html',
             title : 'Monumento ai caduti', 
             lang : 'it'
         })
         .when('/monumentoaicadutidiMusocco', {
             controller : 'cadutidiMusoccoController',
-            templateUrl : 'views/it/content/monumento ai caduti di Musocco.html',
+            templateUrl : 'views/it/luoghi/monumento ai caduti di Musocco.html',
             title : 'Monumento ai caduti di Musocco', 
             lang : 'it'
         })
         .when('/eliminazioneMonumentoaicadutidiMusoccosuWikipedia', {
             controller : 'appController',
-            templateUrl : 'views/it/content/eliminazione Monumento ai caduti di Musocco su Wikipedia.html',
+            templateUrl : 'views/it/luoghi/eliminazione Monumento ai caduti di Musocco su Wikipedia.html',
             title : 'Eliminazione Monumento ai caduti di Musocco su Wikipedia', 
             lang : 'it'
         })
         .when('/eliminazioneMonumentoaicadutidefinizionesuWikipedia', {
             controller : 'appController',
-            templateUrl : 'views/it/content/eliminazione Monumento ai caduti definizione su Wikipedia.html',
+            templateUrl : 'views/it/luoghi/eliminazione Monumento ai caduti definizione su Wikipedia.html',
             title : 'Eliminazione Monumento ai caduti (definizione) su Wikipedia', 
             lang : 'it'
         })
@@ -49,21 +49,21 @@
             title : 'About', 
             lang : 'it'
         })
-        .when('/en-me', {
+        .when('/preamble', {
             controller : 'appController',
-            templateUrl : 'views/en/home/me.html',
-            title : 'Mauro Vezzoli', 
+            templateUrl : 'views/en/home/preamble.html',
+            title : 'Preamble', 
             lang : 'en'
         })
         .when('/en-dante', {
             controller : 'appController',
-            templateUrl : 'views/en/content/dante.html',
+            templateUrl : 'views/en/maestroes/dante.html',
             title : 'Dante', 
             lang : 'en'
         })
         .when('/warmemorialofMusocco', {
             controller : 'cadutidiMusoccoController',
-            templateUrl : 'views/en/content/war memorial of Musocco.html', 
+            templateUrl : 'views/en/places/war memorial of Musocco.html', 
             title:'War Memorial of Musocco',
             lang : 'en'
         })
@@ -74,10 +74,10 @@
             lang : 'en'
         })
         .when('index', {
-            redirectTo : '/me'
+            redirectTo : '/preambolo'
         })
         .otherwise({
-            redirectTo : '/me'
+            redirectTo : '/preambolo'
         });
 }]);
    
